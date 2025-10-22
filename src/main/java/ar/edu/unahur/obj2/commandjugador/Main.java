998565caf80c5f49ee540dd1724640e811b0b4ad
Rodrigo Alarcón <rodricsxd@gmail.com>
2025-10-22 19:53:01 -0300

    Add files via upload

--- src/main/java/ar/edu/unahur/obj2/commandjugador/Main.java
+++ src/main/java/ar/edu/unahur/obj2/commandjugador/Main.java
@@ -8,9 +8,14 @@ public class Main {
         Teclado teclado = new Teclado(jugador);
         Scanner sc = new Scanner(System.in);
         System.out.println(jugador);
-        System.out.print("Ingrese un comando para mover al jugador");
+        System.out.print("Ingrese un comando para mover al jugador o q para salir");
         Character l = sc.next().toLowerCase().charAt(0);
         System.out.println(l);
-        teclado.moverAlPersonaje(l);
+        while (l != 'q') {
+            teclado.moverAlPersonaje(l);
+            System.out.println(jugador);
+            System.out.print("Ingrese otro comando para mover al jugador o q para salir");
+            l = sc.next().toLowerCase().charAt(0);
+        }
     }
 }
